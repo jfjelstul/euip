9b12d558456d889aaa2227b0acfd0bc1949afffa
Josh Fjelstul <joshfjelstul@gmail.com>
2020-06-16 00:39:27 -0500

    Add a function to call the API

--- R/call_API.R
+++ R/call_API.R
@@ -3,20 +3,30 @@
 # euinfr R package
 ###########################################################################
 
-get_case_data <- function() {
+# function to call the EUIP API
+# takes in the query in the form of a string
+# returns a tibble
+call_API <- function(query) {
 
-}
+  # call API
+  response <- httr::GET(query)
 
-get_decision_data <- function() {
+  # convert JSON to tibble
+  out <- jsonlite::fromJSON(rawToChar(response$content))
 
-}
+  # check for API errors
+  if(names(out)[1] == "error") {
+    message <- stringr::str_to_lower(out[1])
+    message <- stringr::str_replace(message, "^the", "The")
+    message <- stringr::str_c("Error from API: ", message, collapse = "")
+    stop(message)
+  }
 
-get_MS_statistics <- function() {
+  # convert to a tibble
+  out <- dplyr::as_tibble(out)
 
-}
-
-get_DG_statistics <- function() {
-
+  # return a tibble
+  return(out)
 }
 
 ###########################################################################
